f5e0980d3a2f82a1a5067fa0e82937bbd6e401e2
SEary342 <45857544+SEary342@users.noreply.github.com>
2020-02-26 08:27:38 -0500

    Changed dictionary and improved search function

--- anagram.py
+++ anagram.py
@@ -1,10 +1,10 @@
 def main():
-    word_set = open_src_dict("popular.txt")
+    word_set = open_src_dict("enable1.txt")
     exact_order_mode = startup_prompt()
+    letters = ""
     while True:
-        letters = str(
-            input("Enter letters to search for or (exit/reset) commands: ").strip()
-        ).lower()
+        if letters == "" or not exact_order_mode:
+            letters = available_letters_prompt()
         if letters == "exit":
             break
         elif letters == "reset":
@@ -12,14 +12,31 @@ def main():
             continue
         else:
             letters = list(letters)
-        letter_combos = get_words(letters, word_set, exact_order_mode)
+        letter_order = []
+        if exact_order_mode:
+            letter_order = str(
+                input("Enter word to search for (?'s for unknown letters): ")
+            ).lower()
+            if letter_order == "exit":
+                break
+            elif letter_order == "reset":
+                exact_order_mode = startup_prompt()
+                continue
+            letter_order = list(letter_order)
+            if len(letter_order) > len(letters):
+                print(
+                    "The provided word to search for must not be longer than the"
+                    + "available letters"
+                )
+                continue
+        letter_combos = get_words(letters, word_set, exact_order_mode, letter_order)
         print(f"\nFound words:\n{letter_combos}\n")
 
 
 def startup_prompt():
     while True:
         exact_order_mode = str(
-            input("\nExact length and order search mode (include ?'s) Y/(N): ")
+            input("\nExact length and order search mode Y/(N): ")
         ).lower()
         if exact_order_mode in ["", "n"]:
             exact_order_mode = False
@@ -32,8 +49,17 @@ def startup_prompt():
     return exact_order_mode
 
 
-def get_words(letters: list, word_set: set, exact_order: bool = False) -> dict:
-    max_word_len = len(letters)
+def available_letters_prompt():
+    return str(input("Enter letters to search for: ").strip()).lower()
+
+
+def get_words(
+    letters: list, word_set: set, exact_order: bool = False, letter_order: list = []
+) -> dict:
+    if exact_order:
+        max_word_len = len(letter_order)
+    else:
+        max_word_len = len(letters)
 
     words = {k: [] for k in range(3, max_word_len + 1)}
     for k, v in words.items():
@@ -43,10 +69,14 @@ def get_words(letters: list, word_set: set, exact_order: bool = False) -> dict:
 
         for item in base_list:
             local_letters = letters.copy()
+            local_order = letter_order.copy()
             letter_list = list(item.lower())
             for i, letter in enumerate(letter_list):
                 if exact_order:
-                    if letter == local_letters[i] or local_letters[i] == "?":
+                    if letter in local_letters and (
+                        letter == local_order[i] or local_order[i] == "?"
+                    ):
+                        local_letters.remove(letter)
                         if i == len(letter_list) - 1:
                             words[k].append(item)
                         else:
@@ -56,8 +86,6 @@ def get_words(letters: list, word_set: set, exact_order: bool = False) -> dict:
                 else:
                     if letter in local_letters:
                         local_letters.remove(letter)
-                    elif "?" in local_letters:
-                        local_letters.remove("?")
                     else:
                         break
                     if i == len(letter_list) - 1:
